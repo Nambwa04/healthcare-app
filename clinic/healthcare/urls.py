47bdebd03028395068ab3666c4e06889c66c2d98
Nambwa04 <bnambwa@kabarak.ac.ke>
2024-09-24 17:41:11 +0300

    Add registration and login functionality

--- clinic/healthcare/urls.py
+++ clinic/healthcare/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
    path('', views.home, name='home'),
+   path('register.html', views.register, name='register'),
+   path('login.html', views.login_user, name='login'),
    path('contact.html', views.contact, name='contact'),
    path('about.html', views.about, name='about'),
    path('departments.html', views.departments, name='departments'),
    path('doctors.html', views.doctors, name = 'doctors'),
+   
 ]
--- clinic/healthcare/views.py
+++ clinic/healthcare/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.http import HttpResponse
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists.")
+
+        else:
+            user = User.objects.create_user(username=username, password=password, email=email)
+            user.save()
+            messages.success(request, "Account created successfully.")
+            return redirect('login')
+        
+    return render(request, 'register.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+
+        user = authenticate(username=username, password=password, email=email)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful.")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid credentials.")
+            return redirect('login')
+    return render(request, 'login.html', {})
+
 def home(request):
     return render(request, 'home.html', {})
 
